e6ea45ea8fdda0b54099d84dfec87cee7bad1daa
Nithin vs <nithin.vs1720@gmail.com>
2023-08-27 13:45:36 +0530

    Done some works on page design

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
-export const Todo = () => {
-    return (
-        <div className="Todo">
-            <p>Go to School</p>
-            <div>
-                <FontAwesomeIcon icon={faPenToSquare} />
-                <FontAwesomeIcon icon={faTrash} />
-            </div>
+export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+ 
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
         </div>
-    )
+    </div>
+  )
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,15 +1,22 @@
-import React, {useState} from "react";
-export const TodoForm = ({addTodo}) => { 
-    const [value, setValue] = useState(" ")
-    const handlesubmit = e => {
+import React, {useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+      // prevent default action
         e.preventDefault();
-        addTodo(value);
-        setValue("")
-    }
-    return (
-        <form className="TodoForm" onSubmit={handlesubmit}>
-            <input type="text" className="todo-input" value={value} placeholder="What is the task today?" onChange={(e) => setValue(e.target.value)} />
-            <button type="submit" className="todo-btn" >Add Task</button>
-        </form>
-    )
+        if (value) {
+          // add todo
+          addTodo(value);
+          // clear form after submission
+          setValue('');
+        }
+      };
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
+    <button type="submit" className='todo-btn'>Add Task</button>
+  </form>
+  )
 }
