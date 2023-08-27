0c12f084ce388f7067ead0bf4e3a8fb65b0012fd
Nithin vs <nithin.vs1720@gmail.com>
2023-08-27 14:33:20 +0530

    Changed something

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -11,8 +11,8 @@ export const EditTodoForm = ({editTodo, task}) => {
       };
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
-    <button type="submit" className='todo-btn'>Add Task</button>
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update Task' />
+    <button type="submit" className='todo-btn'>Update Task</button>
   </form>
   )
-}
+} 
